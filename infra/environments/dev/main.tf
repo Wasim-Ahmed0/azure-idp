25e1f544a6b80829c9aaa41ea81ed1a2aa4d5b10
Wasim Ahmed <wasim.ahmed00@icloud.com>
2026-03-03 14:47:29 +0000

    feat: call platform-network module

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -47,6 +47,21 @@ locals {
   # Core Platform Resource Group
   rg_platform_core = "${var.name_prefix}-rg-platform-core-${var.env}"
 
+  # Network Platform Resource Group
+  rg_platform_network = "${var.name_prefix}-rg-platform-network-${var.env}"
+
+  # Hub Virtual Network
+  hub_vnet_name       = "vnet-${var.name_prefix}-hub-${var.env}"
+  hub_address_space   = ["10.0.0.0/16"]
+
+  # Workloads (spoke) Virtual Network
+  workloads_vnet_name     = "vnet-${var.name_prefix}-workloads-${var.env}"
+  workloads_address_space = ["10.1.0.0/16"]
+  workloads_subnets = {
+    "snet-appint-${var.env}" = { address_prefixes = ["10.1.1.0/24"] } 
+    "snet-pe-${var.env}"     = { address_prefixes = ["10.1.2.0/24"] } 
+  }
+
   tags = {
     project = "azure-idp"
     env     = var.env
@@ -66,6 +81,22 @@ module "platform_core" {
   tags = local.tags
 }
 
+module "platform_network" {
+  source = "../../modules/platform_network"
+
+  location            = var.location
+  resource_group_name = local.rg_platform_network
+
+  hub_vnet_name      = local.hub_vnet_name
+  hub_address_space  = local.hub_address_space
+
+  workloads_vnet_name      = local.workloads_vnet_name
+  workloads_address_space  = local.workloads_address_space
+  workloads_subnets        = local.workloads_subnets
+
+  tags = local.tags
+}
+
 output "platform_core_rg" {
   value = module.platform_core.resource_group_name
 }
@@ -76,4 +107,12 @@ output "acr_login_server" {
 
 output "key_vault_name" {
   value = module.platform_core.key_vault_name
+}
+
+output "network_rg" {
+  value = module.platform_network.resource_group_name
+}
+
+output "workloads_subnet_ids" {
+  value = module.platform_network.workloads_subnet_ids
 }
